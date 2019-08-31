93e43f0812fd2907bc92c6c0d3cd0a5cec8ded56
Dianna Tingg <dtingg@gmail.com>
2019-08-30 19:53:29 -0700

    added tests for all attributes in market init

--- lib/market.rb
+++ lib/market.rb
@@ -4,10 +4,16 @@
 
 module FarMar
   class Market
-    attr_reader :id
+    attr_reader :id, :name, :address, :city, :county, :state, :zip
     
     def initialize(id, name, address, city, county, state, zip)
       @id = id
+      @name = name
+      @address = address
+      @city = city
+      @county = county
+      @state = state
+      @zip = zip
     end
     
     
--- specs/market_spec.rb
+++ specs/market_spec.rb
@@ -12,9 +12,41 @@ describe "Market" do
       market = FarMar::Market.new(id, "People's Co-op Farmers Market", "30th and Burnside", "Portland", "Multnomah", "Oregon", "97202")
       market.must_respond_to :id
       market.id.must_equal id
-      
     end
     
+    it "Keeps track of address" do
+      address = "test"
+      market = FarMar::Market.new(1, "People's Co-op Farmers Market", address, "Portland", "Multnomah", "Oregon", "97202")
+      market.must_respond_to :address
+      market.address.must_equal address
+    end
     
+    it "Keeps track of city" do
+      city = "test"
+      market = FarMar::Market.new(1, "People's Co-op Farmers Market", "30th and Burnside", city, "Multnomah", "Oregon", "97202")
+      market.must_respond_to :city
+      market.city.must_equal city
+    end
+    
+    it "Keeps track of county" do
+      county = "test"
+      market = FarMar::Market.new(1, "People's Co-op Farmers Market", "30th and Burnside", "Portland", county, "Oregon", "97202")
+      market.must_respond_to :county
+      market.county.must_equal county
+    end
+    
+    it "Keeps track of state" do
+      state = "test"
+      market = FarMar::Market.new(1, "People's Co-op Farmers Market", "30th and Burnside", "Portland", "Multnomah", state, "97202")
+      market.must_respond_to :state
+      market.state.must_equal state
+    end
+    
+    it "Keeps track of zip" do
+      zip = "98136"
+      market = FarMar::Market.new(1, "People's Co-op Farmers Market", "30th and Burnside", "Portland", "Multnomah", "Oregon", zip)
+      market.must_respond_to :zip
+      market.zip.must_equal zip
+    end
   end
 end
